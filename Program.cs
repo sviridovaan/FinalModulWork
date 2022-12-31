d4010821a4a80fc825373943b7eeb9dcf1cc08f6
Anna <sviridova_anya@mail.ru>
2022-12-31 20:27:58 +0300

    Вводные данные

--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿//Написать программу, которая из имеющегоcя массива строк 
+//формирует массив из строк, длина которых меньше либо равна 3 символа
+Console.Clear();
+
+string[] array = new string[4] { "hello", "2", "world", ":-)" };
+
+Console.WriteLine($"Заданный массив: ");
+Console.Write($"[");
+ShowArray(array);
+Console.WriteLine($"]");
+
+Console.WriteLine("Элементы, длина которых меньше либо равна 3 символам: ");
+Console.Write($"[");
+PrintArray(array);
+Console.Write($"]");
+
+
+
